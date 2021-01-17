dc498c17ccfbb8fa1a549c0b645d5b2845493039
kamakiri1225 <kusumuratakuya@yahoo.co.jp>
2021-01-17 22:37:19 +0900

    linbot update complete

--- app/views.py
+++ app/views.py
@@ -14,40 +14,50 @@ import pya3rt
 # LINE Messaging APIは、CHANNEL_ACCESS_TOKENとCHANNEL_SECRETが必要
 line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN) # 各API通信を行うときに使用
 handler = WebhookHandler(settings.CHANNEL_SECRET) # 署名の検証で使用
-# talk_api = settings.TALK_API # 雑談BotAPI
+talk_api = settings.TALK_API # 雑談BotAPI
 
-
-# Create your views here.
 class CallbackView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('OK')
 
     def post(self, request, *args, **kwargs):
+        # リクエストヘッダーから署名検証のための値を取得
         signature = request.META['HTTP_X_LINE_SIGNATURE']
+        # リクエストボディを取得
         body = request.body.decode('utf-8')
 
         try:
-            hander.hander(body, signature)
+            # 署名を検証して、問題なければhandleに定義されている関数を呼び出す
+            handler.handle(body, signature)
         except InvalidSignatureError:
+            # 署名検証で失敗した場合は、例外をあげる
             return HttpResponseBadRequest()
         except LineBotApiError as e:
+            # APIのエラーが発生した場合は、例外をあげる
             print(e)
+            return HttpResponseServerError()
 
+        # 処理が成功したらOKを表示
         return HttpResponse('OK')
 
+    # 外部からのアクセスを可能にする
+    # csrf_tokenを渡していないpostメソッドは403エラーになるため
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(CallbackView, self).dispatch(*args, **kwargs)
 
+    # staticmethod関数は、インスタンス化せずに呼び出せる関数のこと
+    # handlerのaddメソッドで、リクエストのイベント毎に実行する関数を記述
     @staticmethod
     @handler.add(MessageEvent, message=TextMessage)
     def message_event(event):
-        reply = event.message.text
+        # オウムが返しする
+        # reply = event.message.text
 
         # 雑談Bot
-        # client = pya3rt.TalkClient(talk_api)
-        # response = client.talk(event.message.text)
-        # reply = response['results'][0]['reply']
+        client = pya3rt.TalkClient(talk_api)
+        response = client.talk(event.message.text)
+        reply = response['results'][0]['reply']
 
         line_bot_api.reply_message(
             event.reply_token,
